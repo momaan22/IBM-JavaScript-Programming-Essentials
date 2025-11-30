b8eac0de50515e4c00c8a7aac9d149919f1666c4
Mohamed Maan <200016023@must.edu.eg>
2025-11-30 07:50:51 -0500

    Use ternary operator

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -49,3 +49,9 @@ switch (userType) {
 }
 
 console.log("User Category:", userCategory);
+
+//Step 5: Use ternary operator for isAuthenticated and authenticationStatus
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
+console.log("Authentication Status:", authenticationStatus);
